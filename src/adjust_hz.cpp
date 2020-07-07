2929afac8437150b401aef247f57a7dbbe607f9e
tiger0421 <taiger0421@yahoo.co.jp>
2020-07-07 23:32:47 +0900

    unify notation publisher->pub sub->sub

--- src/adjust_hz.cpp
+++ src/adjust_hz.cpp
@@ -13,7 +13,7 @@ class My_Filter {
 
      private:
         ros::NodeHandle node_;
-        ros::Publisher scan_publisher_;
+        ros::Publisher scan_pub_;
         ros::Subscriber scan_sub_;
         ros::Subscriber flg_sub_;
 };
@@ -21,7 +21,7 @@ class My_Filter {
 My_Filter::My_Filter(){
         flg_sub_ = node_.subscribe<std_msgs::Bool> ("/turn_flg", 1, &My_Filter::flgCallback, this);
         scan_sub_ = node_.subscribe<sensor_msgs::LaserScan> ("/scan", 1, &My_Filter::scanCallback, this);
-        scan_publisher_ = node_.advertise<sensor_msgs::LaserScan> ("/scan2", 1, false);
+        scan_pub_ = node_.advertise<sensor_msgs::LaserScan> ("/scan2", 1, false);
 }
 
 int My_Filter::get_ros_param(void){
@@ -37,7 +37,7 @@ void My_Filter::flgCallback(const std_msgs::Bool::ConstPtr& flg){
 
 void My_Filter::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
     if(!turn_flg_){
-        scan_publisher_.publish(*scan);
+        scan_pub_.publish(*scan);
     }
 }
 
