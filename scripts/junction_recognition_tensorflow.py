54125d06036f10940b6334281df502c889759f35
tiger0421 <taiger0421@yahoo.co.jp>
2020-07-08 00:09:06 +0900

    change pub Float32MultiArray->String

--- scripts/junction_recognition_tensorflow.py
+++ scripts/junction_recognition_tensorflow.py
@@ -2,7 +2,7 @@
 import rospy
 import rosparam
 from sensor_msgs.msg import LaserScan
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import String
 import numpy as np
 
 import tensorflow as tf
@@ -16,8 +16,8 @@ class Variables:
         rospy.loginfo("start init")
 
         self.loop_rate = rospy.Rate(1)
-        self.h_idx = Float32MultiArray()
-        self.publisher = rospy.Publisher('hypothesis', Float32MultiArray, queue_size=1)
+        self.h_idx=np.zeros(1)
+        self.publisher = rospy.Publisher('hypothesis', String, queue_size=1)
 
         self.TRAIN_DATA_FILES = ['dead_end', 'left', 'right', 'straight', 'threeway_left', 'threeway_center', 'threeway_right']
         NUM_CLASSES = len(self.TRAIN_DATA_FILES)
@@ -32,14 +32,15 @@ class Variables:
         start = time.time()
         input_pointclouds = np.array(data.ranges, dtype=np.float32)
 
-        self.h_idx.data = variables.model.predict(input_pointclouds.reshape(1, len(input_pointclouds)))
-        self.publisher.publish(self.h_idx)
+        self.h_idx = np.array(variables.model.predict(input_pointclouds.reshape(1, len(input_pointclouds))))
+        hypothesis = variables.TRAIN_DATA_FILES[np.argmax(self.h_idx)]
+        self.publisher.publish(hypothesis)
         self.loop_rate.sleep()
 
         end = time.time()
         print("time: ", end-start)
 
-        rospy.loginfo(variables.TRAIN_DATA_FILES[np.argmax(self.h_idx.data)])
+        rospy.loginfo(hypothesis)
 
 if __name__ == '__main__':
     try:
