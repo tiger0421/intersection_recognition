b594ba9543ea52dc0263be2df562c97c90eea77e
tiger0421 <taiger0421@yahoo.co.jp>
2020-07-07 22:30:21 +0900

    add turn_flg to stop publishing /scan2

--- src/adjust_hz.cpp
+++ src/adjust_hz.cpp
@@ -1,20 +1,25 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
+#include "std_msgs/Bool.h"
 #include <unistd.h>
 
 class My_Filter {
      public:
         My_Filter();
+        bool turn_flg_ = false;
         int get_ros_param(void);
+        void flgCallback(const std_msgs::Bool::ConstPtr& flg);
         void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
 
      private:
         ros::NodeHandle node_;
         ros::Publisher scan_publisher_;
         ros::Subscriber scan_sub_;
+        ros::Subscriber flg_sub_;
 };
 
 My_Filter::My_Filter(){
+        flg_sub_ = node_.subscribe<std_msgs::Bool> ("/turn_flg", 1, &My_Filter::flgCallback, this);
         scan_sub_ = node_.subscribe<sensor_msgs::LaserScan> ("/scan", 1, &My_Filter::scanCallback, this);
         scan_publisher_ = node_.advertise<sensor_msgs::LaserScan> ("/scan2", 1, false);
 }
@@ -26,8 +31,14 @@ int My_Filter::get_ros_param(void){
     return hz;
 }
 
+void My_Filter::flgCallback(const std_msgs::Bool::ConstPtr& flg){
+    turn_flg_ = flg->data;
+}
+
 void My_Filter::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
-    scan_publisher_.publish(*scan);
+    if(!turn_flg_){
+        scan_publisher_.publish(*scan);
+    }
 }
 
 int main(int argc, char** argv){
